e413835776e694d214ba3f52cec1918a7c69afb6
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-08-25 11:51:34 +0200

    toying around

--- graph/load_dummy.ml
+++ graph/load_dummy.ml
@@ -5,7 +5,18 @@ open Printf
 open Ekey
 
 let () =
-  let sexp_list = load_rev_sexps "sks_dump.sexp" in
+  let filename = Sys.argv.(1) in
+  let sexp_list = load_rev_sexps filename in
   let ekey_list = List.map ekey_of_sexp sexp_list in
-    printf "loaded %d ekeys from dump file\n" (List.length ekey_list)
+    begin
+      Gc.full_major ();
+      printf "loaded %d ekeys from dump file\n" (List.length ekey_list);
+      ignore (read_line ());
+    end
+    ;
+    let compare_ekey ek1 ek2 =
+      compare ek1.key_keyid ek2.key_keyid
+    in
+    let a = List.sort ?cmp:(Some compare_ekey) ekey_list in
+      printf "list length %d \n" (List.length a)
 ;;
