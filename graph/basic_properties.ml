caf08dd45ba8b2e4338008dcaf1cb20714e62fa2
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-09-14 14:25:28 +0200

    this is a _directed_ graph, so we should observe the in-degree and out-degree *bash*

--- graph/basic_properties.ml
+++ graph/basic_properties.ml
@@ -6,21 +6,28 @@ open Misc
 open Ekey
 open Wot_graph
 
-let count_degrees g output =
-  let deg_map = Map.IntMap.empty in
-  let deg_map = G.fold_vertex
-      (fun v m -> 
-	 let deg = List.length (G.succ g v) in
-	   if deg = 14642 then
-	     print_endline (keyid_to_string v.key_keyid);
+let degree_distribution g in_output out_output =
+  let (indeg_map, outdeg_map) = G.fold_vertex
+      (fun v (in_map, out_map) -> 
+	 let outdeg = G.out_degree g v in
+	 let indeg = G.in_degree g v in
+	 let out_map =
 	   try
-	     Map.IntMap.add deg ((Map.IntMap.find deg m) + 1) m
-	   with Not_found -> Map.IntMap.add deg 1 m
+	     Map.IntMap.add outdeg ((Map.IntMap.find outdeg out_map) + 1) out_map
+	   with Not_found -> Map.IntMap.add outdeg 1 out_map
+	 in
+	 let in_map =
+	   try 
+	     Map.IntMap.add indeg ((Map.IntMap.find indeg in_map) + 1) in_map
+	   with Not_found -> Map.IntMap.add indeg 1 in_map
+	 in
+	   (in_map, out_map)
       )
       g
-      deg_map
+      (Map.IntMap.empty, Map.IntMap.empty)
   in
-    Map.IntMap.iter (fun deg count -> fprintf output "%d %d\n" deg count) deg_map 
+    Map.IntMap.iter (fun deg count -> fprintf out_output "%d %d\n" deg count) outdeg_map;
+    Map.IntMap.iter (fun deg count -> fprintf in_output "%d %d\n" deg count) indeg_map
 
 let () =
   print_endline "compute basic properties of wot graph";
@@ -28,7 +35,8 @@ let () =
   let edge_fname = Sys.argv.(2) in
   let storeable_g = time_evaluation (fun () -> load_storeable_graph_from_files vertex_fname edge_fname) "load_storeable_graph" in
   let g = time_evaluation (fun () -> create_graph storeable_g) "create_graph" in
-  let output = File.open_out "deg.plot" in
-    count_degrees g output
+  let indeg_output = File.open_out "indeg.plot" in
+  let outdeg_output = File.open_out "outdeg.plot" in
+    degree_distribution g indeg_output outdeg_output
     
 
