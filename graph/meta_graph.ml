05b1e59a9c7d781ea1c2e853869bcb357702951a
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-10-07 19:44:41 +0200

    start of graph type definition for the metagraph.

---
+++ graph/meta_graph.ml
@@ -0,0 +1,18 @@
+(* metavertex *)
+module MV = struct
+  type t = int * int ref
+  let compare = fun (id1, _) (id2, _) -> compare id1 id2
+  let hash = fun (id, _) -> id
+  let equal = fun (id1, _) (id2, _) -> id1 = id2
+end
+
+(* metaedge *)
+module ME = struct
+  type label = int
+  let compare = fun w1 w2 -> compare w1 w2
+  let default = 0
+end
+
+(* module M = Imperative.S.ConcreteLabeled(MV) *)
+
+let () = print_endline "foo"
