bd8ec2399172db587a8c78bdb486920b6d395c57
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-10-07 18:07:04 +0200

    functorize graph helper methods.

--- graph/graph_misc.ml
+++ graph/graph_misc.ml
@@ -1,5 +1,12 @@
 open Batteries
 open Printf
+open Graph
+
+module type G = sig
+  type t
+  module V : Sig.COMPARABLE
+  val iter_vertex : (V.t -> unit) -> t -> unit
+end
 
 let write_distribution_to_file enum fname = 
   let f out =
@@ -11,3 +18,22 @@ let intmap_increase_or_add map key =
   try 
     Map.IntMap.add key ((Map.IntMap.find key map) + 1) map
   with Not_found -> Map.IntMap.add key 1 map
+
+
+
+module Graph_helpers(G : G) = struct
+
+  exception Abort
+  let take_some_vertex g =
+    let v = ref None in
+    let f u = 
+      match !v with
+	| Some v -> raise Abort
+	| None -> v := Some u
+    in
+      try 
+	G.iter_vertex f g;
+	None
+      with Abort -> !v
+
+end
