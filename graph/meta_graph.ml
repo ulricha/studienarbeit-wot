03beedaf7ecd7d02e8e4a402704b41fd90205a0a
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-10-12 11:24:20 +0200

    create metagraph, print out basic properties.

--- graph/meta_graph.ml
+++ graph/meta_graph.ml
@@ -1,5 +1,11 @@
 open Batteries
 open Graph
+open Wot_graph
+open Misc
+open Graph_misc
+open Printf
+
+module C = Component_helpers.Make(G)
 
 (* metavertex *)
 module MV = struct
@@ -113,4 +119,27 @@ module Make(G : Sig.G) = struct
       metagraph
 end
 
-let () = print_endline "foo"
+module M = Make(G)
+module Statistics = Network_statistics.Make(MG)
+
+let () =
+  if (Array.length Sys.argv) <> 3 then
+    begin
+      print_endline "usage: basic_properties vertex.mar edges.mar";
+      exit (-1)
+    end
+  else
+    begin
+      print_endline "compute basic properties of wot graph";
+      let vertex_fname = Sys.argv.(1) in
+      let edge_fname = Sys.argv.(2) in
+      let l = fun () -> load_storeable_graph_from_files vertex_fname edge_fname in
+      let storeable_g = time_evaluation l "load_storeable_graph" in
+      let c = fun () -> graph_from_storeable_graph storeable_g in
+      let g = time_evaluation c "graph_from_storeable_graph" in
+      let scc_list = time_evaluation (fun () -> C.scc_list g) "scc_list" in
+      let metagraph = (time_evaluation (fun () -> M.metagraph g scc_list) "metagraph") in
+	Statistics.basic_network_statistics metagraph "metagraph"
+    end
+
+
