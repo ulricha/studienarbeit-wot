2b4b6ad5eb75655b556fe44383d464e954726a0d
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-10-14 15:12:28 +0200

    begin of a mpi implementation of betweeness (not finished).

---
+++ graph/betweeness_mpi.ml
@@ -0,0 +1,80 @@
+open Batteries
+
+open Printf
+open Graph
+open Misc
+open Graph_misc
+open Ekey
+open Wot_graph
+
+module C = Component_helpers.Make(G)
+module B = Betweeness.Make(G)
+
+let sort_alist_by_value l =
+  let cmp (k1, v1) (k2, v2) = compare v1 v2 in
+    List.sort ~cmp:(compare_reverse cmp) l
+
+let betweeness_centrality betweeness_function g name cnt =
+  let betweeness_values = betweeness_function g cnt in
+  let l = List.of_enum (B.H.enum betweeness_values) in
+  let sorted = sort_alist_by_value l in
+  let (keyid, value) = List.hd sorted in
+    printf "most central key in %s: %s (%f)\n" name (keyid_to_string keyid) value
+
+let combine_hashtbl_enum h e =
+  let add_or_create (key, value) =
+    try
+      B.H.replace h key ((B.H.find h key) + value)
+    with Not_found -> B.H.add h key value
+  in
+    Enum.iter add_or_create e
+
+let distribute_work g numworkers =
+  let v_list = G.fold_vertex (fun v l -> v :: l) g [] in
+  let nr_per_worker = (List.length v_list) / (numworkers - 1) in
+  let rec divide_and_send_work worker list =
+    match List.length list with
+      | 0 -> ()
+      | l when l > nr_per_worker ->
+	  let (workunit, rest) = List.split_at nr_per_worker list in
+	    printf "server: send workunit of size %d to worker %d\n" nr_per_worker worker;
+	    Mpi.send workunit worker 0 Mpi.comm_world;
+	    divide_and_send_work (worker + 1) rest
+      | l -> 
+	  printf "server: send workunit of size %d to worker %d\n" l worker;
+	  Mpi.send list worker 0 Mpi.comm_world
+  in
+    divide_and_send_work 1 v_list
+
+let server g =
+  let numworkers = Mpi.comm_size Mpi.comm_world -1 in
+    distribute_work g numworkers
+    
+let worker g =
+  let rank = Mpi.comm_rank Mpi.comm_world in
+  let work = Mpi.receive 0 0 Mpi.comm_world in
+  let msg = sprintf "worker %d: workunit size %d" rank (List.length work) in
+    ignore g;
+    print_endline msg
+
+let load_mscc v_fname e_fname =
+  let storeable_g = load_structinfo_from_files v_fname e_fname in
+  let g = graph_from_structinfo storeable_g in
+  let scc_list = C.scc_list g in
+  let scc_list_sorted = list_list_sort_reverse scc_list in
+    C.graph_from_node_list (List.hd scc_list_sorted) g
+
+(* mscc = maximum strongly connected component *)
+let () =
+  if (Array.length Sys.argv) <> 3 then
+    begin
+      print_endline "usage: basic_properties vertex.sexp edge.sexp";
+      exit (-1)
+    end
+  else
+    let mscc = load_mscc Sys.argv.(1) Sys.argv.(2) in
+      if Mpi.comm_rank Mpi.comm_world = 0 then
+	server mscc
+      else
+	worker mscc;
+      Mpi.barrier Mpi.comm_world
