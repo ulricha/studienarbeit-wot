e1e15e0bd8e6e9920f73324ddc056d6863b8b307
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-11-03 14:15:26 +0100

    remove small components to make the metagraph drawable.

--- graph/meta_graph.ml
+++ graph/meta_graph.ml
@@ -70,50 +70,19 @@ module Make(G : Sig.G) = struct
 
   (* *) 
   let construct_metagraph_nodes component_list component_to_id = 
-    let mv_1 = (1, ref 0) in
-    let mv_2 = (2, ref 0) in
-    let mv_3 = (3, ref 0) in
-    let mv_4 = (4, ref 0) in
-    let i = ref 5 in
+    let i = ref 0 in
     let h = Hashtbl.create (List.length component_list) in
     (* create a metagraph which has nodes on the order of the number 
        of components minus the number of components of size 1 and 2
        which are contracted to one node *)
     let mg = MG.create ~size:((List.length component_list) - 217000) () in
-      MG.add_vertex mg mv_1;
-      MG.add_vertex mg mv_2;
-      MG.add_vertex mg mv_3;
-      MG.add_vertex mg mv_4;
       let add_metavertex component =
 	let component_id = component_to_id component in
 	let n = List.length component in
-	  if n = 1 then
-	    begin
-	      incr (snd mv_1);
-	      Hashtbl.add h component_id mv_1
-	    end
-	  else if n = 2 then
-	    begin
-	      incr (snd mv_2);
-	      Hashtbl.add h component_id mv_2
-	    end
-	  else if n = 3 then
-	    begin
-	      incr (snd mv_3);
-	      Hashtbl.add h component_id mv_3
-	    end
-	  else if n = 4 then
-	    begin
-	      incr (snd mv_4);
-	      Hashtbl.add h component_id mv_4
-	    end
-	  else
-	    begin
-	      incr i;
-	      let mv = (!i, ref n) in
-		MG.add_vertex mg mv;
-		Hashtbl.add h component_id mv
-	    end
+	  incr i;
+	  let mv = (!i, ref n) in
+	    MG.add_vertex mg mv;
+	    Hashtbl.add h component_id mv
       in
 	List.iter add_metavertex component_list;
 	(mg, h)
@@ -149,6 +118,24 @@ end
 module M = Make(G)
 module Statistics = Network_statistics.Make(MG)
 
+exception Invalid_arg
+
+(* assume component list sorted reverse by size *)
+let remove_small_components g component_list =
+  let rec loop cl small_components =
+    match cl with
+      | c :: tl when List.length c <= 10 ->
+	  loop tl (c :: small_components)
+      | c :: tl ->
+	  (small_components, tl)
+      | [] ->
+	  raise Invalid_arg
+  in
+  let (small_components, large_components) = loop component_list [] in
+  let remove_nodes component = List.iter (fun v ->  G.remove_vertex g v) component in
+    List.iter remove_nodes small_components;
+    large_components
+
 let () =
   if (Array.length Sys.argv) <> 3 then
     begin
@@ -160,13 +147,11 @@ let () =
       print_endline "compute basic properties of wot graph";
       let vertex_fname = Sys.argv.(1) in
       let edge_fname = Sys.argv.(2) in
-      let l = fun () -> load_structinfo_from_files vertex_fname edge_fname in
-      let storeable_g = time_eval l "load_storeable_graph" in
-      let c = fun () -> graph_from_structinfo storeable_g in
-      let g = time_eval c "graph_from_storeable_graph" in
-      let scc_list = time_eval (fun () -> C.scc_list g) "scc_list" in
+      let (g, scc_list) = time_eval (fun () -> Component_helpers.load_scc_list vertex_fname edge_fname) "scc_list" in
+      let large_components = remove_small_components g scc_list in
       let metagraph = (time_eval (fun () -> M.metagraph g scc_list) "metagraph") in
       let oc = Pervasives.open_out "metagraph.dot" in 
+	ignore large_components;
 	Dot.output_graph oc metagraph;
 	Pervasives.close_out oc
     end
