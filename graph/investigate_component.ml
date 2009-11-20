3f406b72d1a7adf6ee5c2c18dbe0f1593541dddd
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-11-20 13:15:40 +0100

    debug.

--- graph/investigate_component.ml
+++ graph/investigate_component.ml
@@ -73,14 +73,16 @@ let print_key_records l =
     List.iter print l
 
 let creation_time ctimes =
-  print_endline "creation_time";
-  let ctimes = List.sort ctimes in
-  let a = Array.of_list ctimes in
-  let l = Array.length a in
-  let median = Array.get a (l / 2) in
-  let newest = Array.get a (l - 1) in
-  let oldest = Array.get a 0 in
-    (median, oldest, newest)
+    print_endline "creation_time";
+    let ctimes = List.sort ctimes in
+    let a = Array.of_list ctimes in
+    let l = Array.length a in
+      Printf.printf "len %d\n" l;
+    let median = Array.get a (l / 2) in
+    let newest = Array.get a (l - 1) in
+    let oldest = Array.get a 0 in
+      (median, oldest, newest)
+(*  with _ -> (0., 0., 0.)*)
 
 let print_statistics l =
   let uids = List.map (fun (_, uid, _, _) -> uid) l in
