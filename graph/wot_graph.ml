00e3d899141c4d3d8ead72667ab9f7f9abb6b558
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-12-11 11:43:32 +0100

    unused code.

--- graph/wot_graph.ml
+++ graph/wot_graph.ml
@@ -34,24 +34,6 @@ let directed_to_undirected g =
 let load_structinfo_from_files vertex_filename edge_filename =
   let edges = List.map edgelist_per_vertex_of_sexp (load_rev_sexps edge_filename) in
     ([], edges)
-
-let graph_to_structinfo g =
-  let vertex_list = G.fold_vertex (fun v l -> v :: l) g [] in
-  let edge_map = G.fold_edges
-    (fun signer signee map -> 
-       if Key_map.mem signee map then
-	 let old_entry = Key_map.find signee map in
-	 let (_, l) = old_entry in
-	 let new_entry = (signee, signer :: l) in
-	   Key_map.add signee new_entry map
-       else
-	 let entry = (signee, [signer]) in
-	 Key_map.add signee entry map
-    )
-    g
-    Key_map.empty
-  in
-    (vertex_list, List.of_enum (Key_map.values edge_map))
 	 
 let add_edges g edge_list =
   let add_edges_from_one_vertex edges =
