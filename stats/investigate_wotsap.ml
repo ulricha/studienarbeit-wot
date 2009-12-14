a36e6d61e516a5d3c19dcf0446bb341089b2f901
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-12-14 19:01:40 +0100

    fix width of short keyid strings, print uid.

--- stats/investigate_wotsap.ml
+++ stats/investigate_wotsap.ml
@@ -7,8 +7,8 @@ let _ =
       exit 1
     end
 
-let search_in_keyids l short_keyid =
-  let short_keyid_hex = Printf.sprintf "%lX" short_keyid in
+let search_in_keyids names l i short_keyid =
+  let short_keyid_hex = Printf.sprintf "%08lX" short_keyid in
   let rec loop l =
     match l with
       | hd :: tl ->
@@ -17,7 +17,7 @@ let search_in_keyids l short_keyid =
 	  else
 	    loop tl
       | [] ->
-	  Printf.printf "%s not in my data\n" short_keyid_hex
+	  Printf.printf "%s (%s)\n" short_keyid_hex (Dyn_array.get names i)
   in
     loop l
       
@@ -25,9 +25,9 @@ let main () =
   let dbh = PGOCaml.connect ~database:Sys.argv.(1) () in
   let keyids = Db_interface.get_mscc_keys dbh in
     print_endline "fetched keyids from db";
-  let (_, wotsap_keyids, _) = Wotsap_parser.read_wotsap_file Sys.argv.(2)  in
+  let (names, wotsap_keyids, _) = Wotsap_parser.read_wotsap_file Sys.argv.(2)  in
     Printf.printf "%d %d\n" (List.length keyids) (Dyn_array.length wotsap_keyids);
-    Dyn_array.iter (search_in_keyids keyids) wotsap_keyids
+    Dyn_array.iteri (search_in_keyids names keyids) wotsap_keyids
 
 let _ =
     try main () with e -> prerr_endline (Printexc.to_string e)
