3d47169ede33bae2e81502b3e884bf175b62bd7e
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2009-12-16 17:44:53 +0100

    small tool to print shortest paths between keys.

---
+++ graph/print_path.ml
@@ -0,0 +1,38 @@
+open Batteries
+open Graph
+open Wot_graph
+
+module W = struct
+  type label = G.E.label
+  type t = int
+  let weight l = 1
+  let compare = Pervasives.compare
+  let add = (+)
+  let zero = 0
+end
+
+module D = Path.Dijkstra(G)(W)
+
+let _ = 
+  if Array.length Sys.argv <> 4 then (
+    print_endline "usage: print_path edge_file source target";
+    exit 1)
+
+let main () =
+  let source = Sys.argv.(2) in
+  let target = Sys.argv.(3) in
+  let g = load_graph_from_file Sys.argv.(1) in
+    Printf.printf "nb_vertex %d nb_edge %d\n" (G.nb_vertex g) (G.nb_edges g);
+    try
+      let (path, length) = D.shortest_path g source target in
+	Printf.printf "found shortest path of length %d\n" length;
+	let print_edge e = 
+	  let t = (G.E.dst e) in
+	    Printf.printf "%s %s\n" t (String.sub t 8 8) 
+	in
+	  List.iter print_edge path
+    with Not_found -> print_endline "no path"
+
+let _ =
+  try main () with e -> prerr_endline (Printexc.to_string e)
+    
