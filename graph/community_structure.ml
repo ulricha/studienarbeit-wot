2ec8b59a5d3b5143a4c843a25ed988e96ac9aef2
Alexander Ulrich <ulricha@eap111055.extern.uni-tuebingen.de>
2010-01-21 14:55:52 +0100

    filter communities by size

--- graph/community_structure.ml
+++ graph/community_structure.ml
@@ -7,19 +7,23 @@ open Community_helpers
 module MG_make = Metagraph.Make(G)
 
 let _ =
-  if (Array.length Sys.argv) <> 4 then (
-    print_endline "usage: investigate_communities edge-file index-file community-file";
+  if (Array.length Sys.argv) <> 5 then (
+    print_endline "usage: investigate_communities edge-file index-file community-file minsize";
     exit (-1))
 
 let component_metagraph g cid_map =
+  let minsize = int_of_string Sys.argv.(4) in
   let communities = Map.IntMap.fold (fun i c l -> c :: l) cid_map [] in
-  let c = Metagraph.filter g communities in
-    printf "filtered %d keys\n" c;
-    let metagraph = (time_eval (fun () -> MG_make.metagraph g communities) "metagraph") in
-      print_endline (sprintf "vertices %d edges %d" (Metagraph.MG.nb_vertex metagraph) (Metagraph.MG.nb_edges metagraph));
-      let basename = "metagraph-communities" in
-	Metagraph.export_metagraph_cfinder metagraph (basename ^ ".cyto");
-	Metagraph.export_metagraph_attributes metagraph (basename ^ "_attributes.cyto")
+  printf "len communities %d\n" (List.length communities);
+    let communities = List.filter (fun l -> List.length l >= minsize) communities in
+      printf "len communities %d\n" (List.length communities);
+      let c = Metagraph.filter g communities in
+	printf "filtered %d keys\n" c;
+	let metagraph = (time_eval (fun () -> MG_make.metagraph g communities) "metagraph") in
+	  print_endline (sprintf "vertices %d edges %d" (Metagraph.MG.nb_vertex metagraph) (Metagraph.MG.nb_edges metagraph));
+	  let basename = sprintf "metagraph-communities-%d" minsize in
+	    Metagraph.export_metagraph_cfinder metagraph (basename ^ ".cyto");
+	    Metagraph.export_metagraph_attributes metagraph (basename ^ "_attributes.cyto")
 
 let main () =
   print_endline "construct communities metagraph";
