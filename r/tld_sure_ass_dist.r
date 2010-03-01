9832f1176d1f4ed8bd2cab6cf2c02a3de3e0a0e4
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2010-03-01 11:54:40 +0100

    R: Zuweisung (TLD, SLD, Zeit) abhaengig von Groesse.

---
+++ r/tld_sure_ass_dist.r
@@ -0,0 +1,11 @@
+fdir <- "~/dev/studienarbeit-wot/results/fastmod/"
+ldir <- "~/dev/studienarbeit-wot/results/label-prop/"
+cdir <- "~/dev/studienarbeit-wot/results/copra/"
+fastmod <- scan(paste(fdir, "TLD_SURE_sizes.dat", sep=""))
+label <- scan(paste(ldir, "TLD_SURE_sizes.dat", sep=""))
+copra <- scan(paste(cdir, "TLD_SURE_sizes.dat", sep=""))
+plot(table(fastmod), log="x", type="p", pch=0, xlim=range(fastmod, label, copra), ylim=range(table(fastmod), table(label), table(copra)), col="red",  xlab="Grösse", ylab="Anzahl")
+points(table(label), type="p", pch=1, col="green")
+points(table(copra), type="p", pch=2, col="blue")
+legend(x="topright", legend=c("FM", "LP", "COPRA"), col=c("red", "green", "blue"), pch=c(0,1,2))
+quartz.save("dev/studienarbeit-wot/ausarbeitung/images/tld_sure_dist.pdf", type="pdf")
