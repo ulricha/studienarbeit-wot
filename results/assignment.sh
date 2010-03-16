0383ae554af276771fad80c12433a223698f30f0
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2010-03-16 12:47:36 +0100

    blondel/infomap data, assignment.sh

--- results/assignment.sh
+++ results/assignment.sh
@@ -1,10 +1,10 @@
-for alg in fastmod label-prop cliquemod-3000 cliquemod-500 copra; do
+for alg in fastmod label-prop blondel-l2 blondel-l5 copra infomap_dir; do
     COMNUM=`grep SIG_TIME_CORR $alg/inv.log | wc -l`
     echo $alg size $COMNUM
     for target in TLD_SURE_ASS TLD_MAYBE_ASS SLD_SURE_ASS SLD_MAYBE_ASS; do
 	COUNT=`grep $target $alg/inv.log | wc -l`
 	echo $alg $target $COUNT `echo "scale=3; $COUNT/$COMNUM" | bc`
-	grep $target $alg/inv.log | awk '{ print $3 }' > $alg/$target_sizes.dat
+	grep $target $alg/inv.log | awk '{ print $3 }' > $alg/${target}_sizes.dat
     done
 
     COUNT=`grep SLD_FREEMAIL_ASS $alg/inv.log | wc -l`
