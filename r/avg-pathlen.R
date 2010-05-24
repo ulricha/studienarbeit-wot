f90199d6c178aed025ba50e6afc568b816dde26e
Alexander Ulrich <ulricha@informatik.uni-tuebingen.de>
2010-05-24 20:27:04 +0200

    kleine korrekturen, korrekte legende in abbildungen.

--- r/avg-pathlen.R
+++ r/avg-pathlen.R
@@ -1,3 +1,3 @@
 pl <- scan("dev/studienarbeit-wot/results/basic-properties/scc-44952_avg_dist.values")
-plot(table(pl), xlab="durchschnittliche Pfadlänge", ylab="Anzahl")
+plot(table(pl), xlab="durchschnittliche Distanz", ylab="Anzahl")
 quartz.save("/Users/ulricha/dev/studienarbeit-wot/ausarbeitung/images/pathlen-dist.pdf")
